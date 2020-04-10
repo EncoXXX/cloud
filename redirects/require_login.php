dd17b74a06d006f99c9127ff0498cd9339836c53
EncoXXX <bednarsasha@gmail.com>
2020-04-10 21:42:55 +0300

    Закоментив редірект на логін і дозволив ttf(шрифти) в nginx.conf

--- redirects/require_login.php
+++ redirects/require_login.php
@@ -2,22 +2,22 @@
   $id = Session::getId();
   $url = URL::getUrl();
   if($id == false){
-    if($url == "/login/"){
-      return;
-    }
-    if($url == "/register/"){
-      return;
-    }
-    header("Location: /login");
+    // if($url == "/login/"){
+    //   return;
+    // }
+    // if($url == "/register/"){
+    //   return;
+    // }
+    // header("Location: /login");
   }else{
-    if($url == "/login/"){
-      header("Location: /");
-      exit();
-    }
-    if($url == "/register/"){
-      header("Location: /");
-      exit();
-    }
+    // if($url == "/login/"){
+    //   header("Location: /");
+    //   exit();
+    // }
+    // if($url == "/register/"){
+    //   header("Location: /");
+    //   exit();
+    // }
     User::setUserById($id);
   }
 
