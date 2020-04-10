cbbcf0066ea7e19f9b3a3f064584315208010839
EncoXXX <bednarsasha@gmail.com>
2020-04-10 21:43:32 +0300

    chmod 777 + правки Кості

--- templates/skeleton.php
+++ templates/skeleton.php
@@ -1,5 +1,8 @@
 <html>
 <head>
+<link href="resource\css\jquery-ui.min.css" rel="stylesheet">
+<link href="resource\css\bootstrap.min.css" rel="stylesheet">
+<link href="resource\css\bootstrap.min.css.map" rel="stylesheet">
   <?//Додаємо файли css
   foreach (CSS::getMinFiles() as $path) {
     ?><link rel="stylesheet" href="<?=$path?>"><?
@@ -9,6 +12,10 @@
 <body>
   <?=View::getTemplate("templates/header.php");?>
   <?=View::getHtml();?>
+  <script src="resource\js\external\jquery\jquery.js"></script>
+  <script src="resource\js\jquery-ui.js"></script>
+  <script src="resource\js\bootstrap.min.js"></script>
+  <script src="resource\js\bootstrap.min.js.map"></script>
   <?//Додаємо бібліотеки js
   foreach (JS::getLibs() as $lib) {
     ?><script src="<?=$lib?>"></script><?
@@ -18,5 +25,6 @@
     ?><script src="<?=$path?>"></script><?
   }
   ?>
+  
 </body>
 </html>
